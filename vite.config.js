7b13f03921f1b2ed9a38d1829c60a9db8c42e2d0
Lorenzofox3 <laurent34azerty@gmail.com>
2025-06-11 23:14:06 +0200

    change scaffolding

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '',
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+  base: '/cadastre/',
 });
